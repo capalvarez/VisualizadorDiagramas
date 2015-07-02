78ce4ac61d1128329f0819dadcdb085e2834294e
capalvarez <capalvarez.i@gmail.com>
2015-07-02 14:08:57 -0300

    Con puntos en el borde, pero actúa raro en ocasiones...hay que revisar

--- src/generalTools/NonUniformPointGenerator.java
+++ src/generalTools/NonUniformPointGenerator.java
@@ -7,34 +7,110 @@ import utilities.MyPoint;
 public class NonUniformPointGenerator {
 	MyPoint[] points;
 	
-	public NonUniformPointGenerator(double iX,double mX,double iY,double mY,double w,double h){		
+	public NonUniformPointGenerator(double iX,double mX,double iY,double mY,double width,double height){		
 		ArrayList<Double> xValues = new ArrayList<Double>();
-		double current=0;
+		double currentX=0;
 		
-		for(int i=0;current<=w;i++){
-			xValues.add(current);	
-			current = current + iX*Math.pow(mX,i);
+		for(int i=0;currentX<=width;i++){
+			xValues.add(currentX);	
+			
+			if(mX>=1){
+				currentX = currentX + iX*mX*i;
+			}else{
+				currentX = currentX + iX*mX/i;
+			}
+			
 		}
 		
 		ArrayList<Double> yValues = new ArrayList<Double>();
 		double currentY=0;
 		
-		for(int i=0;currentY<=h;i++){
+		for(int i=0;currentY<height;i++){
 			yValues.add(currentY);	
-			currentY = currentY + iY*Math.pow(mY,i);
-		}
-		
-		points = new MyPoint[yValues.size()*xValues.size()];
-		int global = 0;
-		
-		for(int i=0;i<yValues.size();i++){
-			for(int j=0;j<xValues.size();j++){
-				points[global] = new MyPoint(xValues.get(j),yValues.get(i));
-				global++;
+			if(mY>=1){
+				currentY = currentY + iY*mY*i;
+			}else{
+				currentY = currentY + iY*mY/i;
 			}
 		}
+		
+		/*Revisar si es necesario agregar puntos en los bordes*/
+		
+		if(currentY==height && currentX==width){
+			points = new MyPoint[yValues.size()*xValues.size()];
+			
+			int global = 0;
+			
+			for(int i=0;i<yValues.size();i++){
+				for(int j=0;j<xValues.size();j++){
+					points[global] = new MyPoint(xValues.get(j),yValues.get(i));
+					global++;
+				}
+			}
+		}else{
+			if(currentY==height){
+				points = new MyPoint[yValues.size()*xValues.size() + yValues.size()];
+				int global = 0;
+				
+				for(int i=0;i<yValues.size();i++){
+					for(int j=0;j<xValues.size();j++){
+						points[global] = new MyPoint(xValues.get(j),yValues.get(i));
+						global++;
+					}
+				}
+							
+				for(int i=0;i<yValues.size();i++){
+					points[global] = new MyPoint(width,yValues.get(i));
+					global++;
+				}
+				
+			}	
+			
+			if(currentX==width){
+				points = new MyPoint[yValues.size()*xValues.size() + xValues.size()];
+				int global = 0;
+				
+				for(int i=0;i<yValues.size();i++){
+					for(int j=0;j<xValues.size();j++){
+						points[global] = new MyPoint(xValues.get(j),yValues.get(i));
+						global++;
+					}
+				}
+							
+				for(int i=0;i<xValues.size();i++){
+					points[global] = new MyPoint(xValues.get(i),height);
+					global++;
+				}
+			}
+			
+			if(currentX!=width && currentY!=height){
+
+				points = new MyPoint[yValues.size()*xValues.size() + yValues.size() + xValues.size() + 1];
+				int global = 0;
+				
+				for(int i=0;i<yValues.size();i++){
+					for(int j=0;j<xValues.size();j++){
+						points[global] = new MyPoint(xValues.get(j),yValues.get(i));
+						global++;
+					}
+				}
+				
+				for(int i=0;i<xValues.size();i++){
+					points[global] = new MyPoint(xValues.get(i),height);
+					global++;
+				}
+				
+				for(int i=0;i<yValues.size();i++){
+					points[global] = new MyPoint(width,yValues.get(i));
+					global++;
+				}
+			
+				points[global] = new MyPoint(width,height);
+				
+			}
+		}	
 	}
-	
+
 	
 	public MyPoint[] getPoints(){
 		return points;
